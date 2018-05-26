278079ed2527752045a43cf414c879caf35342bc
Colin Brophy <colin@brophys.com>
2018-05-26 22:53:03 +0100

    Finised tut

--- haskell/hwk1.hs
+++ haskell/hwk1.hs
@@ -1,4 +1,5 @@
 import Data.Char (digitToInt)
+-- Following this tutorial http://www.seas.upenn.edu/~cis194/spring13/hw/01-intro.pdf
 
 toDigits :: (Integral a, Show a) => a -> [a]
 toDigits x 
@@ -11,7 +12,7 @@ toDigitsRev = reverse . toDigits
 
 doubleEveryOther :: Num a => [a] -> [a]
 doubleEveryOther = reverse . doubleFromLeft . reverse
-  where doubleFromLeft = zipWith (*) (cycle [1,2])
+  where doubleFromLeft = zipWith (*) $ cycle [1,2]
 
 sumDigits :: (Integral a, Show a) => [a] -> a
 sumDigits = sum . concatMap toDigits
@@ -20,3 +21,9 @@ validate :: (Integral a, Show a) => a -> Bool
 validate = divides 10 . sumDigits . doubleEveryOther . toDigits
   where divides d n = n `mod` d == 0
 
+type Peg = String
+type Move = (Peg, Peg)
+
+hannoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hannoi 0 a b c = []
+hannoi n a b c = hannoi (n - 1) a c b ++ [(a,b)] ++ hannoi (n - 1) c b a
