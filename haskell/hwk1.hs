e2eb78da930cd105f4bb56cdfc4a355401ead096
Colin Brophy <colin@brophys.com>
2018-05-26 19:33:41 +0100

    Code cleanup of card validator

--- haskell/hwk1.hs
+++ haskell/hwk1.hs
@@ -4,30 +4,18 @@ toDigits :: (Integral a, Show a) => a -> [a]
 toDigits x 
   | x > 0 = map (fromIntegral . digitToInt) . show $ x 
   | otherwise = []
--- toDigits x  
---  | x <= 0 = []
---  | otherwise = toDigits (x `div` 10) ++ [x `mod` 10] 
 
 toDigitsRev :: (Integral a, Show a) => a -> [a]
 toDigitsRev = reverse . toDigits
 
 doubleEveryOther :: Num a => [a] -> [a]
--- doubleEveryOther [] = []
--- doubleEveryOther x = doubleEveryOther (init (init x)) ++ [last (init x)] ++ [(last x)  * 2]
--- Much easier to work with lists from the left in haskell (infinte lists etc)
 doubleEveryOther = reverse . doubleFromLeft . reverse
   where doubleFromLeft = zipWith (*) (cycle [1,2])
 
 sumDigits :: [Integer] -> Integer
--- sumDigits [] = 0
--- sumDigits (x:xs) = x + sumDigits xs
 sumDigits = sum . concatMap toDigits
 
-divides :: Integral a => a -> a -> Bool
-divides d n = n `mod` d == 0
-
 validate :: Integer -> Bool
-validate n = divides 10 $ sumDouble $ toDigits n
-  where sumDouble = sumDigits . doubleEveryOther
-
+validate = divides 10 . sumDigits . doubleEveryOther . toDigits
+  where divides d n = n `mod` d == 0
 
